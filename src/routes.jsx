1f29f5a98a5672dff1f5262d59af5fbfd71f18cb
Varma devraj Gandikota <gvarmadevraj369@gmail.com>
2025-02-18 10:22:06 +0530

    last commit

---
+++ src/routes.jsx
@@ -0,0 +1,15 @@
+import { HashRouter, Route, Routes } from "react-router-dom"
+
+
+export const AppRoutes=()=>{
+
+    return(
+        <>
+        <HashRouter>
+            <Routes>
+                
+            </Routes>
+        </HashRouter>
+        </>
+    )
+}
